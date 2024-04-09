1df15d7d1cd1d6b70d4ca1f0e7df98190c2032ed
projectmiluju <dnjsdyd0712@naver.com>
2024-04-09 17:14:02 +0900

    Hi Vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,22 @@
 </script>
 
 <template>
+
+<h1>Hi Vue</h1>
+<hr>
+<h3>상품목록</h3>
+<ul>
+  <li>애플</li>
+  <li>아이폰</li>
+  <li>아이패드</li>
+  <li>맥북</li>
+</ul>
+<h3>Copyright 2024 Vue.js</h3>
+
 </template>
 
 <style scoped>
+h1 {
+  color: brown;
+}
 </style>
