cfe0c83c9770e9c7a818775eb6f324c605d99e0e
projectmiluju <dnjsdyd0712@naver.com>
2024-04-09 18:49:06 +0900

    HTML 클래스 바인딩

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,37 @@
 import { ref } from 'vue';
 
 const title = ref('상품목록..');
-const products = ref(['애플', '아이폰', '아이페드', '에어팟', '맥북']);
+const products_simple = ref(['애플', '아이폰', '아이페드', '에어팟', '맥북']);
+const products = ref([
+  {
+    productId: '0001',
+    title: 'iPhone1',
+    price: 1200,
+    discountRate: 15,
+    isRocket: true
+  },
+  {
+    productId: '0002',
+    title: 'iPhone2',
+    price: 1300,
+    discountRate: 10,
+    isRocket: false
+  },
+  {
+    productId: '0023',
+    title: 'iPhone3',
+    price: 1400,
+    discountRate: 8,
+    isRocket: true
+  },
+  {
+    productId: '0035',
+    title: 'iPhone4',
+    price: 1500,
+    discountRate: 5,
+    isRocket: false
+  },
+]);
 const copyright = ref('Copyright 2024 Vue.js');
 const imgSrc = 'https://w7.pngwing.com/pngs/854/555/png-transparent-vue-js-hd-logo-thumbnail.png'
 const imgWidth = 50;
@@ -24,7 +54,12 @@ function greetButtonClicked() {
 <button @click="greetButtonClicked">누르면 콘솔로 인사를 합니다.</button>
 <h3>{{ title }}</h3>
 <ul>
-  <li v-for="product in products">{{ product }}</li>
+  <li v-for="product in products">
+    {{ product.title}} -
+    ${{ product.price}}
+    ({{ product.price - (product.price * (product.discountRate / 100)) }}) :
+    {{ product.isRocket ? '로켓배송' : '3일후 받아요'}}
+  </li>
 </ul>
 <h3>{{ copyright }}</h3>
 
